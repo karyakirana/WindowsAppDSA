c755085989e3089c18e836d419d4630c14d1ee9c
karyakirana <adibpratama2@gmail.com>
2023-02-03 18:13:15 +0700

    yoman

---
+++ WindowsAppFix1/Modul/MainModule.vb
@@ -0,0 +1,11 @@
+﻿Module MainModule
+
+#Region "Global Variable"
+    ' Every process using global variable
+
+    Public Token As String
+    Public BaseURl As String = "http://erpbsa.test/api/"
+
+#End Region
+
+End Module
---
+++ WindowsAppFix1/Repository/LoginRepository.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Dynamic
+Imports System.Net.Http
+Imports System.Text
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class LoginRepository
+
+    Public _client As HttpClient
+    Public _response As HttpResponseMessage
+    Public _jObject As JObject
+
+    Private _username As String
+    Private _password As String
+
+    Public Sub New()
+        _client = New HttpClient()
+        _client.BaseAddress = New Uri(BaseURl)
+        _client.DefaultRequestHeaders.Accept.Add(
+            New Headers.MediaTypeHeaderValue("application/json")
+        )
+    End Sub
+
+    Public Async Sub Login(email As String, password As String)
+        Dim objectLogin As Object = New ExpandoObject()
+        objectLogin.username = email
+        objectLogin.password = password
+        'serialize
+        Dim json As String = JsonConvert.SerializeObject(objectLogin)
+        Dim content As New StringContent(json, Encoding.UTF8, "application/json")
+        _response = Await _client.PostAsync("login", content)
+
+        If _response.IsSuccessStatusCode Then
+            Dim jsonString As String = Await _response.Content.ReadAsStringAsync
+            _jObject = JsonConvert.DeserializeObject(Of JObject)(jsonString)
+        End If
+
+        If Not _response.IsSuccessStatusCode Then
+
+        End If
+
+    End Sub
+
+End Class

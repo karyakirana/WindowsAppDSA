35296fab390970dd29072b9022b90b3df6af3e5e
kirana <adibpratama2@gmail.com>
2023-02-07 06:35:19 +0700

    yoman

---
+++ WindowsAppFix1/Model/Stock/StockKeluar.vb
@@ -0,0 +1,44 @@
+﻿Public Class StockKeluar
+
+	Public Property id As Long
+	Public Property active_cash As String
+	Public Property stockable_masuk_id As Long?
+	Public Property stockable_masuk_type As String
+	Public Property kode As String
+	Public Property draft As Byte
+	Public Property kondisi As String
+	Public Property status As String
+	Public Property surat_jalan As String
+	Public Property tgl_masuk As String
+	Public Property lokasi_id As Long
+	Public Property supplier_id As Long?
+	Public Property customer_id As Long?
+	Public Property user_id As Long
+	Public Property total_barang As Integer
+	Public Property total_hpp As Integer
+	Public Property keterangan As String
+	Public Property created_at As String
+	Public Property updated_at As String
+	Public Property deleted_at As String
+
+	Public Property lokasi As Lokasi
+	Public Property supplier As Supplier
+	Public Property customer As Customer
+	Public Property stock_keluar_detail As List(Of StockKeluarDetail)
+
+End Class
+
+Public Class StockKeluarDetail
+
+	Public Property id As Long
+	Public Property stock_keluar_id As Long
+	Public Property persediaan_id As Long
+	Public Property harga_beli As Integer
+	Public Property jumlah As Integer
+	Public Property sub_total As Integer
+	Public Property created_at As String
+	Public Property updated_at As String
+
+	Public Property persediaan As Persediaan
+
+End Class
---
+++ WindowsAppFix1/Repository/StockRepo/StockMasukRepository.vb
@@ -0,0 +1,38 @@
+﻿Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Net.Http
+
+Public Class StockMasukRepository
+
+    Public _client As HttpClient
+    Public _response As HttpResponseMessage
+    Public _jObject As JObject
+
+    Public Sub New()
+        _client = New HttpClient With {
+            .BaseAddress = New Uri(BaseURl)
+        }
+        _client.DefaultRequestHeaders.Add("Accept", "application/json")
+        TokenCheck()
+        _client.DefaultRequestHeaders.Authorization = New Headers.AuthenticationHeaderValue("Bearer", Token)
+    End Sub
+
+    Public Async Function GetList() As Task(Of List(Of StockMasuk))
+        _response = Await _client.GetAsync("persediaan/masuk")
+
+        If _response.IsSuccessStatusCode Then
+            Dim jsonString As String = Await _response.Content.ReadAsStringAsync
+            _jObject = JsonConvert.DeserializeObject(Of JObject)(jsonString)
+            Return _jObject("data").ToObject(Of List(Of StockMasuk))
+        End If
+
+        If Not _response.IsSuccessStatusCode Then
+            ResponseException(_response.StatusCode, Await _response.Content.ReadAsStringAsync)
+            Return Nothing
+        End If
+
+        Return Nothing
+
+    End Function
+
+End Class
